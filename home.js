cead5919aabe08df8b0cd655b1673623b02d57a6
BabyManisha <babymaneesha@gmail.com>
2016-12-28 18:39:44 +0530

    completed v1

--- home.js
+++ home.js
@@ -1,33 +1,50 @@
-
 function redirect() {
-   window.location="word.html";
+   // window.location="word.html";
+   $("#login").removeClass("show");
+   $("#login").addClass("hide");
+   $("#wordgame").removeClass("hide");
+   $("#wordgame").addClass("show");
+   displayImage();
+}
+
+function post() {
+    FB.api('/me/feed', 'post', {
+        message : "The Most Romantic Word Between Me and My Soulmate is!!",
+        link: 'https://s3-ap-southeast-1.amazonaws.com/sm-word-game/index.html',
+        picture: 'https://s3-ap-southeast-1.amazonaws.com/sm-word-game/'+path
+    }, function(response) {
+        // document.getElementById('status').innerHTML = response.id;
+        console.log(response);
+        console.log("SM Success!!");
+        window.location = "https://www.facebook.com/";
+    });
 }
 
 // The SDK loaded callback (see below)
 window.fbAsyncInit = function() {
-            // The SDK is loaded so let's init it.
-            FB.init({
-                        appId : '1130006373787712',
-                        xfbml : true,
-                        version : 'v2.8'
-            });
-            // We check the user's login status
-            FB.getLoginStatus(function(response) {
-                        if (response.status === 'connected') {
+      // The SDK is loaded so let's init it.
+      FB.init({
+          appId : '1130006373787712',
+          xfbml : true,
+          version : 'v2.8'
+      });
+      // We check the user's login status
+      FB.getLoginStatus(function(response) {
+            if (response.status === 'connected') {
+                  // displayImage();
+                  redirect();
+            } else {
+                  // If the user is NOT already logged in, we ask him to do it first
+                  FB.login(function(response) {
+                        if (response.authResponse) {
                                     // displayImage();
                                     redirect();
                         } else {
-                                    // If the user is NOT already logged in, we ask him to do it first
-                                    FB.login(function(response) {
-                                                if (response.authResponse) {
-                                                            // displayImage();
-                                                            redirect();
-                                                } else {
-                                                            // User refused to give your site permissions, no friends list !
-                                                }
-                                    });
+                                    // User refused to give your site permissions, no friends list !
                         }
-            });
+                  }, {scope: 'publish_actions'});
+            }
+      });
 };
 
 
@@ -35,10 +52,10 @@ window.fbAsyncInit = function() {
 // Here you will load Facebook's SDK asynchronously (it will not block your page loading)
 // Once the SDK is loaded, it will call the window.fbAsyncInit function above
 (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "//connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
